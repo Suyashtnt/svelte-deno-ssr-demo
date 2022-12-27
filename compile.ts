aa2b7c3ffe473474bdaf08fdfc0cea2c7d6f4c0f
Suyashtnt <suyashtnt@gmail.com>
2022-12-27 23:49:09 +0200

    refactor: add less duplicate code

--- compile.ts
+++ compile.ts
@@ -6,6 +6,7 @@ import sveltePreprocess from "npm:svelte-preprocess";
 import { build, stop } from "https://deno.land/x/esbuild@v0.16.11/mod.js";
 import { httpImports } from "https://deno.land/x/esbuild_plugin_http_imports@v1.2.4/index.ts";
 import sveltePlugin from "npm:esbuild-svelte";
+import { BuildOptions } from "../.cache/deno/npm/registry.npmjs.org/typescript/4.9.4/lib/typescript.d.ts";
 
 const sveltePath = "https://esm.sh/svelte@3.55.0";
 
@@ -14,50 +15,56 @@ const preprocessor = sveltePreprocess();
 
 await Deno.mkdir("bundle", { recursive: true });
 
-const client = await build({
-  plugins: [
-    sveltePlugin({
-      preprocess: preprocessor,
-      compilerOptions: {
-        generate: "dom",
-        hydratable: true,
-        sveltePath,
-      },
-    }),
-    httpImports({
-      defaultToJavascriptIfNothingElseFound: true,
-    }),
-  ],
-  entryPoints: ["./handWritten/client.js"],
+const defaultSvelteOpts = {
+  preprocess: preprocessor,
+  compilerOptions: {
+    generate: "dom",
+    hydratable: true,
+    sveltePath,
+  },
+}
+
+const httpImport = httpImports({
+  defaultToJavascriptIfNothingElseFound: true,
+});
+
+const defaultEsbuildOpts: BuildOptions = {
   mainFields: ["svelte", "browser", "module", "main"],
-  outfile: "./bundle/client.js",
   metafile: true,
   bundle: true,
   minify: true,
   format: "esm",
+};
+
+
+const client = await build({
+  ...defaultEsbuildOpts,
+  plugins: [
+    // @ts-expect-error typescript what cocaine are you on that this isn't callable
+    sveltePlugin(defaultSvelteOpts),
+    httpImport
+  ],
+  entryPoints: ["./handWritten/client.js"],
+  outfile: "./bundle/client.js",
 });
 
 const server = await build({
+  ...defaultEsbuildOpts,
   plugins: [
+    // @ts-expect-error typescript what cocaine are you on that this isn't callable
     sveltePlugin({
-      preprocess: preprocessor,
+      ...defaultSvelteOpts,
       compilerOptions: {
         generate: "ssr",
-        hydratable: true,
-        sveltePath,
       },
     }),
-    httpImports({
-      defaultToJavascriptIfNothingElseFound: true,
-    }),
+    httpImport
   ],
   entryPoints: ["./handWritten/index.svelte"],
-  mainFields: ["svelte", "browser", "module", "main"],
   outfile: "./bundle/ssr.js",
-  metafile: true,
-  bundle: true,
-  minify: true,
-  format: "esm",
 });
+
+await Deno.writeTextFile("./bundle/client.meta.json", JSON.stringify(client.metafile));
+await Deno.writeTextFile("./bundle/ssr.meta.json", JSON.stringify(server.metafile));
 
 stop();
