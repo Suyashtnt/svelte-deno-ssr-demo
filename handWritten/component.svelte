b2ec993528e9ed9f61fb1aea840c06d0f903ceb4
Suyashtnt <suyashtnt@gmail.com>
2022-12-28 10:39:20 +0200

    feat: add confetti URL import test

--- handWritten/component.svelte
+++ handWritten/component.svelte
@@ -1,5 +1,16 @@
 <script lang="ts">
+    import confetti from "https://esm.sh/canvas-confetti@1.6.0"
     import { saying } from "./test.js"
+
+    function runConfetti() {
+        confetti({
+            particleCount: 100,
+            spread: 70,
+            origin: { y: 0.6 }
+        })
+    }
 </script>
 
 <p>{saying}</p>
+
+<button on:click={runConfetti}>Click me</button>
